ca0991527cb4c907deae9d360f11462a19264e69
JosWambugu <josphatwambugu77@gmail.com>
2021-09-09 13:45:02 +0300

    Exception Handler 1

--- src/main/java/com/tutorial/javagraphql/resolvers/bank/ClientResolver.java
+++ src/main/java/com/tutorial/javagraphql/resolvers/bank/ClientResolver.java
@@ -2,6 +2,7 @@ package com.tutorial.javagraphql.resolvers.bank;
 
 import com.tutorial.javagraphql.model.BankAccount;
 import com.tutorial.javagraphql.model.Client;
+import graphql.GraphQLException;
 import graphql.kickstart.tools.GraphQLResolver;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
@@ -16,9 +17,10 @@ public class ClientResolver implements GraphQLResolver<BankAccount> {
 
     public Client clientName(BankAccount bankAccount){
         log.info("Client created with ban account:"+bankAccount.getId());
-        List<String> list =new ArrayList<String>();
-        list.add("Maina");
-        return Client.builder().id(UUID.randomUUID()).firstname("Jos").lastname("Wambugu").middlename(list).build();
+        throw new GraphQLException("Client Unavailable");
+      //  List<String> list =new ArrayList<String>();
+        //list.add("Maina");
+        //return Client.builder().id(UUID.randomUUID()).firstname("Jos").lastname("Wambugu").middlename(list).build();
 
     }
 }
