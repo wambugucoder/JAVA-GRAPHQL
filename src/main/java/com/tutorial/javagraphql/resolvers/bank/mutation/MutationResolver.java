dc984895b936481319c86bb23258299cd31833a0
JosWambugu <josphatwambugu77@gmail.com>
2021-09-10 11:41:25 +0300

    File Upload

--- src/main/java/com/tutorial/javagraphql/resolvers/bank/mutation/MutationResolver.java
+++ src/main/java/com/tutorial/javagraphql/resolvers/bank/mutation/MutationResolver.java
@@ -3,16 +3,27 @@ package com.tutorial.javagraphql.resolvers.bank.mutation;
 import com.tutorial.javagraphql.model.BankAccount;
 import com.tutorial.javagraphql.model.Currency;
 import com.tutorial.javagraphql.request.BankAccountInput;
+import graphql.kickstart.servlet.context.DefaultGraphQLServletContext;
 import graphql.kickstart.tools.GraphQLMutationResolver;
+import graphql.schema.DataFetchingEnvironment;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
+@Slf4j
 public class MutationResolver implements GraphQLMutationResolver {
     public BankAccount createBankAccount(BankAccountInput input){
         return BankAccount.builder().id(UUID.randomUUID()).currency(Currency.USD).build();
 
 
+    }
+    public UUID upload(DataFetchingEnvironment environment){
+        DefaultGraphQLServletContext context= environment.getContext();
+        context.getFileParts().forEach(part -> {
+            log.info("Uploading:{},with size:{}",part.getSubmittedFileName(),part.getSize());
+        });
+        return UUID.randomUUID();
     }
 }
