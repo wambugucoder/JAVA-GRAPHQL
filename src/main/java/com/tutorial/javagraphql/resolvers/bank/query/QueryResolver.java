3121fcf5492e3944cc11d287a6c486f151bed5fe
JosWambugu <josphatwambugu77@gmail.com>
2021-09-13 10:43:30 +0300

    Selection Set

--- src/main/java/com/tutorial/javagraphql/resolvers/bank/query/QueryResolver.java
+++ src/main/java/com/tutorial/javagraphql/resolvers/bank/query/QueryResolver.java
@@ -4,6 +4,7 @@ import com.tutorial.javagraphql.model.BankAccount;
 import com.tutorial.javagraphql.model.Client;
 import com.tutorial.javagraphql.model.Currency;
 import graphql.kickstart.tools.GraphQLQueryResolver;
+import graphql.schema.DataFetchingEnvironment;
 import lombok.extern.slf4j.Slf4j;
 import lombok.var;
 import org.springframework.stereotype.Component;
@@ -16,9 +17,13 @@ import java.util.UUID;
 @Component
 public class QueryResolver implements GraphQLQueryResolver {
 
-    public BankAccount bankAccount(UUID id){
-        log.info("Account created with id:" +id);
+    public BankAccount bankAccount(UUID id, DataFetchingEnvironment environment){
 
+      if(environment.getSelectionSet().contains("currency")){
+          log.info("Field contains Currency ");
+      }
+
+        log.info("Account created with id:" +id);
 
        return BankAccount.builder().id(id).currency(Currency.USD).build();
 
